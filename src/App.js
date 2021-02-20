c1644e5f25953a2a4f450a1fa8a911647320c197
021120 <yongtatt981213@hotmail.com>
2021-02-20 17:38:55 +0800

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
